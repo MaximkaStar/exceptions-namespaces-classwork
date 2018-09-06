e2b00a0134239b2597f6a243f1832a78c9132f6a
Maxim <max.martsinkus@mail.ru>
2018-09-06 17:11:23 +0600

    Initial Commit

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class User
+    {
+        public void CreateData()
+        {
+            // тут происходит какая-то работа и бац.
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ exeptions-namespaces-classwork/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using static System.Console;
+
+namespace exeptions_namespaces_classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            checked
+            { }
+            unchecked
+            { }
+
+            ReadLine();
+
+            try
+            {
+                user.CreateData();
+            }
+            catch (WebException networkException)
+            {
+                WriteLine(networkException.Message);
+            }
+            catch (IOException exception)
+            {
+                WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                WriteLine(exception.Message);
+            }
+            finally { }
+
+            ReadLine();
+        }
+    }
+}
